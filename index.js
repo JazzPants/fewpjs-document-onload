c1250ab9b176fba0473d0cffe7939f4533c20457
JazzPants <leighton.l14@gmail.com>
2021-11-26 16:39:52 +1100

    Done!

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    let newParagraph = document.getElementById("text");
+    newParagraph.innerHTML = "This is really cool!"
+  });
+
+
+
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
+
+  //HTML5 option
+  //<script src="index.js" defer></script>
+  //the JavaScript code stored in index.js will be 
+  //loaded up but won't execute until the HTML page completely loads.
